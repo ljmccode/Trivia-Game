12deaebf32e9e3be3436f4cd4c9efcb80386573c
Laura <lmcsay@gmail.com>
2019-04-15 11:43:29 -0500

    cleaned up html, javascript bug fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
 
-var quiz = [
-    {
-        question: "What was The Lion King originally going to be called?",
-        options: ["King of the Jungle", "Pride Rock", "Circle of Life", "A Lion's Tale"],
-        correct: 0
-    },
-    {
-        question: "How old is Crush in Finding Nemo",
-        options: ["35 years old", "85 years old", "150 years old", "175 years old"],
-        correct: 2
-    },
-    {
-        question: "In Snow White and the Seven Dwarfs, what does the wicked Queen ask the Huntsman to bring back to her to prove Snow White is dead?",
-        options: ["Her finger", "Her necklace", "A lock of her hair", "Her heart"],
-        correct: 3
-    },
-    {
-        question: "The woman who voices Belle from Beauty and the Beast also voices what other lead Disney role?",
-        options: ["Ariel from The Little Mermaid", "Esmeralda from The Hunchback of Norte Dame", "Merida from Brave", "Meg from Hercules"],
-        correct: 3
+    var quiz = [
+        {
+            question: "What was The Lion King originally going to be called?",
+            options: ["King of the Jungle", "Pride Rock", "Circle of Life", "A Lion's Tale"],
+            correct: 0
+        },
+        {
+            question: "How old is Crush in Finding Nemo",
+            options: ["35 years old", "85 years old", "150 years old", "175 years old"],
+            correct: 2
+        },
+        {
+            question: "In Snow White and the Seven Dwarfs, what does the wicked Queen ask the Huntsman to bring back to her to prove Snow White is dead?",
+            options: ["Her finger", "Her necklace", "A lock of her hair", "Her heart"],
+            correct: 3
+        },
+        {
+            question: "The woman who voices Belle from Beauty and the Beast also voices what other lead Disney role?",
+            options: ["Ariel from The Little Mermaid", "Esmeralda from The Hunchback of Norte Dame", "Merida from Brave", "Meg from Hercules"],
+            correct: 3
 
-    },
-    {
-        question: "What is a phrase Woody from Toy Story does NOT say when you pull his string? ",
-        options: ["That's not my horse!", "Reach for the sky!", "Somebody's poisoned the water hole!", "There's a snake in my boot!"],
-        correct: 0
-    }];
+        },
+        {
+            question: "What is a phrase Woody from Toy Story does NOT say when you pull his string? ",
+            options: ["That's not my horse!", "Reach for the sky!", "Somebody's poisoned the water hole!", "There's a snake in my boot!"],
+            correct: 0
+        }];
 
 
 
@@ -40,6 +40,7 @@ var interval;
 var timer = 15;
 var randomQuestion;
 var numberOfQuestions = quiz.length;
+var userGuess = "";
 
 $("#reset").hide();
 // On clicking start button, starts game, starts countdown
@@ -65,7 +66,7 @@ function countdown() {
     if (timer === -1) {
         console.log("stop timer")
         // if timer hits 0, display answer and stop timer
-        $("#answer-results").html("<h3>You ran out of time! The correct answer is " + quiz[correct].options + "</h3>");
+        $("#answersAndResults").html("<h3>You ran out of time! The correct answer is " + randomQuestion.options[randomQuestion.correct] + "</h3>");
         stopTimer();
         unanswered++;
         determineFinish();
@@ -81,35 +82,58 @@ function showQuestion() {
     // random question chosen from quiz array
     randomQuestion = quiz[Math.floor(Math.random() * numberOfQuestions)];
     console.log(randomQuestion.question);
+    console.log("Correct answer is " + randomQuestion.correct);
     // displays question
     $("#question").html("<h3>" + randomQuestion.question + "<h3>");
-    // creating buttons for each answer choice and adding classes
+    // creating divs for each answer choice, adding classes, adding questions to divs
     for (var i = 0; i < randomQuestion.options.length; i++) {
-        var answers = $("<button>")
+        var answers = $("<div>")
         answers.addClass("answerOption");
         answers.html(randomQuestion.options[i]);
         console.log(randomQuestion.options[i]);
         // assigning different attribute to each answer option to distiguish
         answers.attr("data-value", i);
         // appending answer options to site
-        $("#answer-options").append(answers);
+        $("#answersAndResults").append(answers);
     }
 
+
+    // upon user clicking one of the answer options
+    $(".answerOption").on("click", function() {
+        // store value of user's guess as integer to compare with correct answer
+        userGuess = parseInt($(this).attr("data-value"));
+        console.log("User guess answer is " + userGuess);
+
+        if(userGuess === randomQuestion.correct) {
+            correct++;
+            userGuess = "";
+            stopTimer()
+            $("#answersAndResults").html("<h3>Correct!!</h3>")
+            determineFinish();
+        } else {
+            incorrect++;
+            userGuess = "";
+            stopTimer();
+            $("answersAndResults").html("<h3>Sorry, the correct answer is " + randomQuestion.options[randomQuestion.correct])
+            determineFinish();
+        }
+    
+    })
 }
 
 function determineFinish() {
     // add question asked into questionsAsked array
     questionsAsked.push(randomQuestion);
 
-    var clearAnswers = setTimeout(function() {
-        $("#answerOptions").empty();
+    clearAnswers = setTimeout(function() {
+        $("#answersAndResults").empty();
         timer = 15;
     
-    if (numberOfQuestions <= questionsAsked) {
+    if (correct + incorrect + unanswered === numberOfQuestions) {
         displayFinish();
-    }   
-    if (questionsAsked.indexOf(randomQuestion) !== -1){
-        return showQuestion();
+    } else {
+        startCountdown();
+        showQuestion();
     }
 
 
@@ -120,9 +144,9 @@ function determineFinish() {
 function displayFinish() {
     $("#question").empty();
     $("#question").html("<h3>All done, here's how you did!<h3>")
-    $("#results").append("<h4>Correct: " + correct + "<h4>");
-    $("#results").append("<h4>Incorrect: " + incorrect + "<h4>");
-    $("#results").append("<h4>Unanswerd: "+ unanswered + "<h4>");
+    $("#answersAndResults").append("<h4>Correct: " + correct + "<h4>");
+    $("#answersAndResults").append("<h4>Incorrect: " + incorrect + "<h4>");
+    $("#answersAndResults").append("<h4>Unanswerd: "+ unanswered + "<h4>");
     $("#reset").show();
 }
 
